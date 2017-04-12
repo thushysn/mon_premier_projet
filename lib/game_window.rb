59df56d6517be13fdf9fcb8dbac3964466ff8ef6
julien <julien.orlando.pinna@gmail.com>
2017-04-12 17:30:25 +0200

    mechant
    s^prite mechant

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -7,7 +7,7 @@ class GameWindow < Hasu::Window
     super(WINDOW_X, WINDOW_Y, false)
     @background_sprite = Gosu::Image.new(self, 'images/mako.png', true)
     @koala_sprite = Gosu::Image.new(self, 'images/essai.png', true)
-    @enemy_sprite = Gosu::Image.new(self, 'images/enemy.png', true)
+    @enemy_sprite = Gosu::Image.new(self, 'images/bea.png', true)
     @flag_sprite = Gosu::Image.new(self, 'images/flag.png', true)
     @font = Gosu::Font.new(self, Gosu::default_font_name, 30)
     @flag = {x: WINDOW_X - SPRITE_SIZE, y: WINDOW_Y - SPRITE_SIZE}
